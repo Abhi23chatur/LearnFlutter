c7e6a51b36e286f65ea6af7a56f3162a85c56c5a
Abhijit Chatur <Abhi23chatur>
2022-03-06 13:23:04 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:first8example/pages/home_page.dart';
+import 'package:first8example/pages/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily:GoogleFonts.lato().fontFamily,
+          primaryTextTheme: GoogleFonts.latoTextTheme()
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+     // initialRoute: "/home",
+      routes: {
+      "/": (context) => LoginPage(),
+     "/home": (context) => HomePage(),
+      "/login": (context) =>  LoginPage(),
+
+      },
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Abhishek";
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body : Center(
+        child: Container(
+          child: Text("Welcome to $days of Flutter by  $name "),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+
+    throw UnimplementedError();
+
+  }
+
+
+  }
